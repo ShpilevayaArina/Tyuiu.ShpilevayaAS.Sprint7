22b8f342e0001ececa7c27b299b9cce9e2859d27
Tyuiu.ShpilevayaAS@outlook.com <rina.ress@mail.ru>
2023-12-13 23:18:46 +0500

    13.12 update

--- Tyuiu.ShpilevayaAS.Sprint7.Project.V2/FormMain.Designer.cs
+++ Tyuiu.ShpilevayaAS.Sprint7.Project.V2/FormMain.Designer.cs
@@ -29,23 +29,141 @@ namespace Tyuiu.ShpilevayaAS.Sprint7.Project.V2
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.BorderlessForm_SAS = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.pictureBoxLogo_SAS = new System.Windows.Forms.PictureBox();
+            this.labelLogo_SAS = new System.Windows.Forms.Label();
+            this.tabListOfSections_SAS = new Guna.UI2.WinForms.Guna2TabControl();
+            this.tabPageDepartments_SAS = new System.Windows.Forms.TabPage();
+            this.tabPageEmployees_SAS = new System.Windows.Forms.TabPage();
+            this.tabPageProviders_SAS = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo_SAS)).BeginInit();
+            this.tabListOfSections_SAS.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // BorderlessForm_SAS
+            // 
+            this.BorderlessForm_SAS.BorderRadius = 30;
+            this.BorderlessForm_SAS.ContainerControl = this;
+            this.BorderlessForm_SAS.DockIndicatorTransparencyValue = 0.6D;
+            this.BorderlessForm_SAS.TransparentWhileDrag = true;
+            // 
+            // pictureBoxLogo_SAS
+            // 
+            this.pictureBoxLogo_SAS.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLogo_SAS.Image")));
+            this.pictureBoxLogo_SAS.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxLogo_SAS.Name = "pictureBoxLogo_SAS";
+            this.pictureBoxLogo_SAS.Size = new System.Drawing.Size(60, 53);
+            this.pictureBoxLogo_SAS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogo_SAS.TabIndex = 0;
+            this.pictureBoxLogo_SAS.TabStop = false;
+            // 
+            // labelLogo_SAS
+            // 
+            this.labelLogo_SAS.AutoSize = true;
+            this.labelLogo_SAS.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelLogo_SAS.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(210)))), ((int)(((byte)(65)))));
+            this.labelLogo_SAS.Location = new System.Drawing.Point(78, 37);
+            this.labelLogo_SAS.Name = "labelLogo_SAS";
+            this.labelLogo_SAS.Size = new System.Drawing.Size(454, 28);
+            this.labelLogo_SAS.TabIndex = 1;
+            this.labelLogo_SAS.Text = "корпоративное приложение сети магазинов";
+            // 
+            // tabListOfSections_SAS
+            // 
+            this.tabListOfSections_SAS.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.tabListOfSections_SAS.Controls.Add(this.tabPageDepartments_SAS);
+            this.tabListOfSections_SAS.Controls.Add(this.tabPageEmployees_SAS);
+            this.tabListOfSections_SAS.Controls.Add(this.tabPageProviders_SAS);
+            this.tabListOfSections_SAS.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tabListOfSections_SAS.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.tabListOfSections_SAS.ItemSize = new System.Drawing.Size(180, 90);
+            this.tabListOfSections_SAS.Location = new System.Drawing.Point(0, 84);
+            this.tabListOfSections_SAS.Name = "tabListOfSections_SAS";
+            this.tabListOfSections_SAS.SelectedIndex = 0;
+            this.tabListOfSections_SAS.Size = new System.Drawing.Size(1157, 602);
+            this.tabListOfSections_SAS.TabButtonHoverState.BorderColor = System.Drawing.Color.Empty;
+            this.tabListOfSections_SAS.TabButtonHoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(59)))));
+            this.tabListOfSections_SAS.TabButtonHoverState.Font = new System.Drawing.Font("Segoe UI Semibold", 10F);
+            this.tabListOfSections_SAS.TabButtonHoverState.ForeColor = System.Drawing.Color.White;
+            this.tabListOfSections_SAS.TabButtonHoverState.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.tabListOfSections_SAS.TabButtonIdleState.BorderColor = System.Drawing.Color.Empty;
+            this.tabListOfSections_SAS.TabButtonIdleState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(59)))));
+            this.tabListOfSections_SAS.TabButtonIdleState.Font = new System.Drawing.Font("Segoe UI Semibold", 10F);
+            this.tabListOfSections_SAS.TabButtonIdleState.ForeColor = System.Drawing.Color.White;
+            this.tabListOfSections_SAS.TabButtonIdleState.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(59)))));
+            this.tabListOfSections_SAS.TabButtonSelectedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(59)))));
+            this.tabListOfSections_SAS.TabButtonSelectedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(210)))), ((int)(((byte)(65)))));
+            this.tabListOfSections_SAS.TabButtonSelectedState.Font = new System.Drawing.Font("Segoe UI Semibold", 10F);
+            this.tabListOfSections_SAS.TabButtonSelectedState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(59)))));
+            this.tabListOfSections_SAS.TabButtonSelectedState.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(210)))), ((int)(((byte)(65)))));
+            this.tabListOfSections_SAS.TabButtonSize = new System.Drawing.Size(180, 90);
+            this.tabListOfSections_SAS.TabIndex = 2;
+            this.tabListOfSections_SAS.TabMenuBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(59)))));
+            // 
+            // tabPageDepartments_SAS
+            // 
+            this.tabPageDepartments_SAS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(59)))));
+            this.tabPageDepartments_SAS.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabPageDepartments_SAS.Location = new System.Drawing.Point(184, 4);
+            this.tabPageDepartments_SAS.Name = "tabPageDepartments_SAS";
+            this.tabPageDepartments_SAS.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageDepartments_SAS.Size = new System.Drawing.Size(969, 594);
+            this.tabPageDepartments_SAS.TabIndex = 0;
+            this.tabPageDepartments_SAS.Text = "Магазины";
+            // 
+            // tabPageEmployees_SAS
+            // 
+            this.tabPageEmployees_SAS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(59)))));
+            this.tabPageEmployees_SAS.Location = new System.Drawing.Point(184, 4);
+            this.tabPageEmployees_SAS.Name = "tabPageEmployees_SAS";
+            this.tabPageEmployees_SAS.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageEmployees_SAS.Size = new System.Drawing.Size(969, 594);
+            this.tabPageEmployees_SAS.TabIndex = 1;
+            this.tabPageEmployees_SAS.Text = "Сотрудники";
+            // 
+            // tabPageProviders_SAS
+            // 
+            this.tabPageProviders_SAS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(59)))));
+            this.tabPageProviders_SAS.Location = new System.Drawing.Point(184, 4);
+            this.tabPageProviders_SAS.Name = "tabPageProviders_SAS";
+            this.tabPageProviders_SAS.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageProviders_SAS.Size = new System.Drawing.Size(969, 594);
+            this.tabPageProviders_SAS.TabIndex = 2;
+            this.tabPageProviders_SAS.Text = "Поставщики";
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1139, 639);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(59)))));
+            this.ClientSize = new System.Drawing.Size(1157, 686);
+            this.Controls.Add(this.tabListOfSections_SAS);
+            this.Controls.Add(this.labelLogo_SAS);
+            this.Controls.Add(this.pictureBoxLogo_SAS);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MinimumSize = new System.Drawing.Size(1157, 686);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Сеть магазинов";
             this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo_SAS)).EndInit();
+            this.tabListOfSections_SAS.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm BorderlessForm_SAS;
+        private System.Windows.Forms.Label labelLogo_SAS;
+        private System.Windows.Forms.PictureBox pictureBoxLogo_SAS;
+        private Guna.UI2.WinForms.Guna2TabControl tabListOfSections_SAS;
+        private System.Windows.Forms.TabPage tabPageDepartments_SAS;
+        private System.Windows.Forms.TabPage tabPageEmployees_SAS;
+        private System.Windows.Forms.TabPage tabPageProviders_SAS;
     }
 }
 
