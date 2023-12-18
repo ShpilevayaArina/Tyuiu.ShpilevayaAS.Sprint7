753b369aba46e6ec5536cd2dd4b0294947156c21
Tyuiu.ShpilevayaAS@outlook.com <rina.ress@mail.ru>
2023-12-18 20:59:47 +0500

    18.12 update

--- Tyuiu.ShpilevayaAS.Sprint7.Project.V2.Lib/DataService.cs
+++ Tyuiu.ShpilevayaAS.Sprint7.Project.V2.Lib/DataService.cs
@@ -9,5 +9,41 @@ namespace Tyuiu.ShpilevayaAS.Sprint7.Project.V2.Lib
 {
     public class DataService
     {
+        public string CollectTextFromFile(string path, int row, int column)
+        {
+            string fileData = File.ReadAllText(path);
+            fileData = fileData.Replace('\n', '\r');
+            string[] lines = fileData.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int rows = lines.Length;
+            int columns = lines[0].Split(';').Length;
+
+            string[,] matrix = new string[rows, columns];
+
+            for (int r = 0; r < rows; r++)
+            {
+                string[] line_r = lines[r].Split(';');
+                for (int c = 0; c < columns; c++)
+                {
+                    matrix[r, c] = Convert.ToString(line_r[c]);
+                }
+            }
+
+            string res = "";
+            for (int i = 0; i < rows; i++)
+            {
+                if (i == row)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        if (j == column)
+                        {
+                            res = matrix[i, j];
+                        }
+                    }
+                }
+            }
+            return res;
+        }
     }
 }
