cbda09eb631114c9e8e93c783a5ba908e9205322
Tyuiu.ShpilevayaAS@outlook.com <rina.ress@mail.ru>
2023-12-22 20:00:17 +0500

    22.12 update

--- Tyuiu.ShpilevayaAS.Sprint7.Project.V2.Lib/DataService.cs
+++ Tyuiu.ShpilevayaAS.Sprint7.Project.V2.Lib/DataService.cs
@@ -9,68 +9,25 @@ namespace Tyuiu.ShpilevayaAS.Sprint7.Project.V2.Lib
 {
     public class DataService
     {
-        public string GetDataFromFile(string row, int column)
+        public string[,] LoadDataFromFile(string path)
         {
-            string path = @"C:\Users\katri\source\repos\Tyuiu.ShpilevayaAS.Sprint7\Tyuiu.ShpilevayaAS.Sprint7.Project.V2.Lib\ListOfDepartments.csv";
+            string[] str = File.ReadAllLines(path);
 
-            string fileData = File.ReadAllText(path);
-            fileData = fileData.Replace('\n', '\r');
-            string[] lines = fileData.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
-
-            int rows = lines.Length;
-            int columns = lines[0].Split(';').Length;
+            int columns = str[0].Split(';').Length;
+            int rows = str.Length;
 
             string[,] matrix = new string[rows, columns];
-            string[] departments = new string[rows];
 
-            for (int r = 0; r < rows; r++)
+            for (int i = 0; i < str.Length; i++)
             {
-                string[] line_r = lines[r].Split(';');
-                for (int c = 0; c < columns; c++)
+                string strIndexI = str[i];
+                string[] strArr = strIndexI.Split(';');
+                for (int c = 0; c < strArr.Length; c++)
                 {
-                    matrix[r, c] = Convert.ToString(line_r[c]);
-                    if (c == 0)
-                    {
-                        departments[r] = Convert.ToString(line_r[c]);
-                    }
+                    matrix[i, c] = strArr[c];
                 }
             }
-
-            string res = "";
-            for (int i = 0; i < rows; i++)
-            {
-                if (i == Array.IndexOf(departments, row))
-                {
-                    for (int j = 0; j < columns; j++)
-                    {
-                        if (j == column)
-                        {
-                            res = matrix[i, j];
-                        }
-                    }
-                }
-            }
-            return res;
-        }
-        public bool AddNewDataToFile(string path, string[] line)
-        {
-            bool completed = false;
-            string str = "";
-
-            for (int i = 0; i < line.Length; i++)
-            {
-                if (i != line.Length - 1)
-                {
-                    str = str + line[i] + ";";
-                }
-                else
-                {
-                    str = str + line[i];
-                }
-            }
-            File.AppendAllText(path, str + Environment.NewLine, Encoding.GetEncoding(1251));
-            completed = true;
-            return completed;
+            return matrix;
         }
     }
 }
